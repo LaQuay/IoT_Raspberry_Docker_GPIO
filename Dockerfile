5cf0d41e23f432757964aa3eba5719345223de0f
Marc Vila <marcvilagomez@gmail.com>
2018-04-01 21:35:32 +0000

    Firebase as API to send working

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,11 @@
 FROM python:3
 MAINTAINER Marc Vila <hola@marcvila.me>
 
-# Install wiringPi
+# Install wiringPi for GPIO
 RUN pip install wiringpi
+
+# Install request for HTTP requests
+RUN pip install requests
 
 ENV LANG C.UTF-8
 ENV PYTHONIOENCODING UTF-8
--- src/my_script.py
+++ src/my_script.py
@@ -1,4 +1,21 @@
 import wiringpi
+import requests
+
+BASE_URL = "https://iot-simple-api.firebaseio.com/"
+
+def sendData(sensor_name, sensor_value):
+    url = BASE_URL + "data.json"
+    data = {
+        "name": sensor_name,
+        "value": sensor_value,
+        "timestamp": ""
+    }
+    print("Sending data..." + url + str(data))
+    print("URL: " + url)
+    print(str(data))
+    
+    r = requests.post(url, json=data)
+    print("Data sent... "+ str(r.status_code) + " " + str(r.reason))
 
 print("Starting script...")
 
@@ -9,3 +26,6 @@ wiringpi.wiringPiSetupGpio()
 pin_value = wiringpi.digitalRead(6)
 
 print("Pin 6: " + str(pin_value))
+
+sendData("Test", "288")
+
