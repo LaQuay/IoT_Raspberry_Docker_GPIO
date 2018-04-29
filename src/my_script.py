65b446ae3d7c2532805fc4b4d0e91cd5bcaced75
Marc Vila <marcvilagomez@gmail.com>
2018-04-29 10:27:27 +0000

    Rain sensor working

--- src/my_script.py
+++ src/my_script.py
@@ -1,31 +1,53 @@
 import wiringpi
 import requests
+from datetime import datetime
+
+# Pinout
+# https://docs.microsoft.com/en-us/windows/iot-core/media/pinmappingsrpi/rp2_pinout.png
 
 BASE_URL = "https://iot-simple-api.firebaseio.com/"
+HIGH = 1
+LOW = 0
+OUTPUT = 1
+INPUT = 0
 
-def sendData(sensor_name, sensor_value):
+def sendData(sensor_name, sensor_value, sensor_observation=""):
     url = BASE_URL + "data.json"
     data = {
         "name": sensor_name,
         "value": sensor_value,
-        "timestamp": ""
+        "timestamp": str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')),
+        "observations": sensor_observation
     }
-    print("Sending data..." + url + str(data))
-    print("URL: " + url)
+    print("Sending data...")
     print(str(data))
     
     r = requests.post(url, json=data)
     print("Data sent... "+ str(r.status_code) + " " + str(r.reason))
+    
+# D0 of sensor placed in GPIO3 (5 pinout)
+# Sends a 1 if rain is detected
+# Sends a 0 if rain is not detected
+def readRainSensor():
+    pin = 3
+    wiringpi.pinMode(pin, INPUT)  
+    pin_value = wiringpi.digitalRead(pin)
+
+    print("Pin " + str(pin) + ": " + str(pin_value))
+    if pin_value:
+        pin_value = 0
+        observation = "No rain detected"
+    else:
+        pin_value = 1
+        observation = "Rain detected"
+    print("Observation: " + str(observation))
+
+    sendData("Rain Sensor", str(pin_value), str(observation))
 
 print("Starting script...")
 
 # Set up GPIO pin numbering
 wiringpi.wiringPiSetupGpio()
 
-# Read pin 6
-pin_value = wiringpi.digitalRead(6)
-
-print("Pin 6: " + str(pin_value))
-
-sendData("Test", "288")
+readRainSensor()
 
